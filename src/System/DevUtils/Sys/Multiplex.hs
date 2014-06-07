f7a1b3808d619b418f7563a4d91a2787a95ed340
Andrew Darqui <andrew.darqui@gmail.com>
2014-06-07 13:41:28 -0400

    unknown

--- src/System/DevUtils/Sys/Multiplex.hs
+++ src/System/DevUtils/Sys/Multiplex.hs
@@ -2,4 +2,4 @@ module System.DevUtils.Sys.Multiplex (
  Multiplexer
 ) where
 
-data Multiplexer = EPOLL | KQUEUE | SELECT | POLL deriving (Show, Read)
+data Multiplexer = EPOLL | KQUEUE | SELECT | POLL | UNKNOWN deriving (Show, Read)
