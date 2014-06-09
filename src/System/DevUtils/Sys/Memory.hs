1a7b691d8f15d9c6272f15402d41c2c3d0191c59
Andrew Darqui <andrew.darqui@gmail.com>
2014-06-09 02:18:34 -0400

    unknown

--- src/System/DevUtils/Sys/Memory.hs
+++ src/System/DevUtils/Sys/Memory.hs
@@ -2,5 +2,5 @@ module System.DevUtils.Sys.Memory (
  Memory(..)
 ) where
 
-data Memory a = B a | K a | M a | G a | T a | P a
+data Memory a = B a | K a | M a | G a | T a | P a | UNKNOWN a
  deriving (Show)
